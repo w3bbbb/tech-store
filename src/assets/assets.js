a374f736a5150c0fc55a864c5b18f781b79b3b6c
w3bbbb <chrissww9@gmail.com>
2025-05-06 15:52:19 -0600

    Add new asset images and Tailwind CSS configuration
    - Added profile_icon.png, quality_icon.png, razorpay_logo.png, search_icon.png, star_dull_icon.png, star_icon.png, stripe_logo.png, and support_img.png to the assets directory.
    - Created a Tailwind CSS configuration file (tailwind.config.js) to set up the project for utility-first styling.

---
+++ src/assets/assets.js
@@ -0,0 +1,57 @@
+import p_img1 from "./p_img1.png";
+
+import logo from "./logo.png";
+import hero_img from "./hero_img.png";
+import cart_icon from "./cart_icon.png";
+import bin_icon from "./bin_icon.png";
+import dropdown_icon from "./dropdown_icon.png";
+import exchange_icon from "./exchange_icon.png";
+import profile_icon from "./profile_icon.png";
+import quality_icon from "./quality_icon.png";
+import search_icon from "./search_icon.png";
+import star_dull_icon from "./star_dull_icon.png";
+import star_icon from "./star_icon.png";
+import support_img from "./support_img.png";
+import menu_icon from "./menu_icon.png";
+import about_img from "./about_img.png";
+import contact_img from "./contact_img.png";
+import razorpay_logo from "./razorpay_logo.png";
+import stripe_logo from "./stripe_logo.png";
+import cross_icon from "./cross_icon.png";
+
+export const assets = {
+  logo,
+  hero_img,
+  cart_icon,
+  dropdown_icon,
+  exchange_icon,
+  profile_icon,
+  quality_icon,
+  search_icon,
+  star_dull_icon,
+  star_icon,
+  bin_icon,
+  support_img,
+  menu_icon,
+  about_img,
+  contact_img,
+  razorpay_logo,
+  stripe_logo,
+  cross_icon,
+};
+
+export const products = [
+  {
+    _id: "aaaaa",
+    name: "Women Round Neck Cotton Top",
+    description:
+      "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+    price: 100,
+    image: [p_img1],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+];
