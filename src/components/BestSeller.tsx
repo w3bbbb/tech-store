a46c043611ec82adc5dea5fca1035c779ab2fb64
w3bbbb <chrissww9@gmail.com>
2025-05-14 18:23:49 -0600

    Add BestSeller component and Product type definition; update LatestCollection component

--- src/components/BestSeller.tsx
+++ src/components/BestSeller.tsx
@@ -1,21 +1,21 @@
 import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import type { Product } from "../types/shopTypes";
 import Title from "./Title";
 import ProductItem from "./ProductItem";
-import type { Product } from "../types/shopTypes";
 
-const LatestCollection = () => {
+const BestSeller = () => {
   const { products } = useContext(ShopContext);
-  const [latestProducts, setLatestProducts] = useState<Product[]>([]);
+  const [bestSeller, setBestSeller] = useState<Product[]>([]);
 
   useEffect(() => {
-    setLatestProducts(products);
+    const bestSellers = products.filter((item: Product) => item.bestseller);
+    setBestSeller(bestSellers.slice(0, 5));
   }, []);
-
   return (
     <div className="my-10">
-      <div className="text-center py-8 text-3xl">
-        <Title text1={"LATEST"} text2={"COLLECTIONS"} />
+      <div className="text-center text-3xl py-8">
+        <Title text1={"BEST"} text2={"SELLERS"} />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
           fringilla, nunc in facilisis efficitur, nisi nunc aliquet nunc, eget
@@ -24,18 +24,14 @@ const LatestCollection = () => {
       </div>
 
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6">
-        {latestProducts.map((item, index) => (
-          <ProductItem
-            key={index}
-            id={item._id}
-            image={item.image}
-            name={item.name}
-            price={item.price}
-          />
-        ))}
+        {
+          bestSeller.map((item, index) => (
+            <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+          ))
+        }
       </div>
     </div>
   );
 };
 
-export default LatestCollection;
+export default BestSeller;
---
+++ src/types/shopTypes.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  _id: string,
+  name: string,
+  description: string,
+  price: number,
+  image: string[],
+  category: string,
+  subCategory: string,
+  sizes: string[],
+  date: string,
+  bestseller: boolean,
+};
