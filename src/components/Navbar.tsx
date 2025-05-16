829b11ac27c639ab1069d448294c9917e5daf40c
w3bbbb <chrissww9@gmail.com>
2025-05-16 17:01:40 -0600

    Refactor Navbar and enhance Login and Orders pages; improve layout and styling, add form handling in Login, and integrate routing in Orders

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,72 +1,133 @@
-import { useContext, useState } from 'react'
-import { assets } from '../assets/assets'
-import { Link, NavLink } from 'react-router-dom'
-import { ShopContext } from '../context/ShopContext'
+import { useContext, useState } from "react";
+import { assets } from "../assets/assets";
+import { Link, NavLink } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
-  const [visible, setVisible] = useState(false)
-  const {setShowSearch, getCartCount} = useContext(ShopContext);
+  const [visible, setVisible] = useState(false);
+  const { setShowSearch, getCartCount } = useContext(ShopContext);
 
   return (
-    <div className='flex items-center justify-between py-5 font-medium'>
-      <Link to='/'><img src={assets.logo} className='max-h-16 min-h-16' alt="logo" /></Link>
+    <div className="flex items-center justify-between py-5 font-medium">
+      <Link to="/">
+        <img src={assets.logo} className="max-h-16 min-h-16" alt="logo" />
+      </Link>
 
-      <ul className='hidden sm:flex gap-5 text-md text-gray-700'>
-        <NavLink to='/' className='flex flex-col items-center gap-1 '>
+      <ul className="hidden sm:flex gap-5 text-md text-gray-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1 ">
           <p>HOME</p>
-          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
-        <NavLink to='/collection' className='flex flex-col items-center gap-1 '>
+        <NavLink to="/collection" className="flex flex-col items-center gap-1 ">
           <p>COLLECTION</p>
-          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
-        <NavLink to='/about' className='flex flex-col items-center gap-1 '>
+        <NavLink to="/about" className="flex flex-col items-center gap-1 ">
           <p>ABOUT</p>
-          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
-        <NavLink to='/contact' className='flex flex-col items-center gap-1 '>
+        <NavLink to="/contact" className="flex flex-col items-center gap-1 ">
           <p>CONTACT</p>
-          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
       </ul>
 
-      <div className='flex items-center gap-6'>
-        <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="search" />
-        <div className='group relative'>
-          <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="profile" />
+      <div className="flex items-center gap-6">
+        <img
+          onClick={() => setShowSearch(true)}
+          src={assets.search_icon}
+          className="w-5 cursor-pointer"
+          alt="search"
+        />
+        <div className="group relative">
+          <Link to='/login'>
+            <img
+              src={assets.profile_icon}
+              className="w-5 cursor-pointer"
+              alt="profile"
+            />
+          </Link>
 
-          <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
-            <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-white text-gray-500 rounded-lg shadow-lg'>
-              <NavLink to='/login' className='hover:text-gray-700'>My Profile</NavLink>
-              <NavLink to='/cart' className='hover:text-gray-700'>Orders</NavLink>
-              <NavLink to='/orders' className='hover:text-gray-700'>Logout</NavLink>
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-white text-gray-500 rounded-lg shadow-lg">
+              <NavLink to="/login" className="hover:text-gray-700">
+                My Profile
+              </NavLink>
+              <NavLink to="/cart" className="hover:text-gray-700">
+                Orders
+              </NavLink>
+              <NavLink to="/orders" className="hover:text-gray-700">
+                Logout
+              </NavLink>
             </div>
           </div>
         </div>
 
-        <Link to='/cart' className='relative'>
-          <img src={assets.cart_icon} className='w-5 min-w-5' alt="cart" />
-          <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>{getCartCount()}</p>
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} className="w-5 min-w-5" alt="cart" />
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+            {getCartCount()}
+          </p>
         </Link>
 
-        <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="menu" />
+        <img
+          onClick={() => setVisible(true)}
+          src={assets.menu_icon}
+          className="w-5 cursor-pointer sm:hidden"
+          alt="menu"
+        />
       </div>
 
       {/* Mobile Menu */}
-      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all duration-700 ${visible ? 'w-full' : 'w-0'} sm:hidden`}>
-        <div className='flex flex-col text-gray-600'>
-          <div onClick={() => setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
-            <img src={assets.dropdown_icon} className='h-4 rotate-180' alt="dropdown" />
+      <div
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all duration-700 ${
+          visible ? "w-full" : "w-0"
+        } sm:hidden`}
+      >
+        <div className="flex flex-col text-gray-600">
+          <div
+            onClick={() => setVisible(false)}
+            className="flex items-center gap-4 p-3 cursor-pointer"
+          >
+            <img
+              src={assets.dropdown_icon}
+              className="h-4 rotate-180"
+              alt="dropdown"
+            />
             <p>Back</p>
           </div>
-          <NavLink onClick={() => setVisible(false)} to='/' className='py-2 pl-6 border' >HOME</NavLink>
-          <NavLink onClick={() => setVisible(false)} to='/collection' className='py-2 pl-6 border' >COLLECTION</NavLink>
-          <NavLink onClick={() => setVisible(false)} to='/about' className='py-2 pl-6 border' >ABOUT</NavLink>
-          <NavLink onClick={() => setVisible(false)} to='/contact' className='py-2 pl-6 border' >CONTACT</NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            to="/"
+            className="py-2 pl-6 border"
+          >
+            HOME
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            to="/collection"
+            className="py-2 pl-6 border"
+          >
+            COLLECTION
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            to="/about"
+            className="py-2 pl-6 border"
+          >
+            ABOUT
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            to="/contact"
+            className="py-2 pl-6 border"
+          >
+            CONTACT
+          </NavLink>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,63 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { useState } from "react";
 
-export default Login
+const Login = () => {
+  const [currentState, setCurrentState] = useState("Login");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="quantico-regular text-3xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+      </div>
+      {currentState === "Login" ? (
+        ""
+      ) : (
+        <input
+          className="w-full px-3 py-2 border rounded-md border-gray-800"
+          placeholder="Name"
+          required
+        />
+      )}
+
+      <input
+        type="email"
+        className="w-full px-3 py-2 border rounded-md border-gray-800"
+        placeholder="Email"
+        required
+      />
+      <input
+        type="password"
+        className="w-full px-3 py-2 border rounded-md border-gray-800"
+        placeholder="Password"
+        required
+      />
+      <div className="w-full flex justify-between text-sm mt-[-8px]">
+        <p className="cursor-pointer">Forgot your password?</p>
+        {currentState === "Login" ? (
+          <p
+            onClick={() => setCurrentState("Sign Up")}
+            className="cursor-pointer"
+          >
+            Create account
+          </p>
+        ) : (
+          <p
+            onClick={() => setCurrentState("Login")}
+            className="cursor-pointer"
+          >
+            Login Here
+          </p>
+        )}
+      </div>
+      <button className="bg-black text-white font-light px-8 py-2 mt-4 rounded-md">
+        {currentState === "Login" ? "Sign In" : "Sign Up"}
+      </button>
+    </form>
+  );
+};
+
+export default Login;
